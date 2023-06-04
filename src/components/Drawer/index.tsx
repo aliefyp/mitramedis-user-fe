8c82e23bf34fa69eb507741129bcf6c9d7a29574
Alief <aliefyp@gmail.com>
2023-06-04 19:39:42 +0700

    fix: change drawer init method

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,4 +1,7 @@
-import { MutableRefObject, ReactNode, useEffect } from "react";
+import { MutableRefObject, ReactNode, useEffect, useState } from "react";
+import { Drawer } from 'flowbite';
+import { FaTimes } from 'react-icons/fa';
+import type { DrawerOptions } from "flowbite";
 
 interface DrawerProps {
   name: string;
@@ -6,19 +9,46 @@ interface DrawerProps {
   trigger: MutableRefObject<HTMLButtonElement | null>;
 }
 
-const Drawer = ({ name, children, trigger }: DrawerProps) => {
+const CustomDrawer = ({ name, children, trigger }: DrawerProps) => {
+  const [isMounted, setMounted] = useState(false);
+
   useEffect(() => {
+    // set attribute to trigger button element
     trigger.current?.setAttribute('data-drawer-target', name);
-    trigger.current?.setAttribute('data-drawer-show', name);
-    trigger.current?.setAttribute('data-drawer-backdrop', 'true');
+    trigger.current?.setAttribute('data-drawer-toggle', name);
+    // trigger.current?.setAttribute('data-drawer-backdrop', 'false');
     trigger.current?.setAttribute('aria-controls', name);
   }, [name, trigger])
+
+  useEffect(() => {
+    if (name && document.getElementById(name) && !isMounted) {
+      // set the drawer menu element
+      const targetEl: HTMLElement = document.getElementById(name) as HTMLElement;
+
+      // options with default values
+      const options: DrawerOptions = {
+        placement: 'left',
+        backdrop: true,
+        bodyScrolling: false,
+        edge: false,
+        edgeOffset: '',
+        backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-30',
+        onHide: () => { },
+        onShow: () => { },
+        onToggle: () => { }
+      };
+
+      new Drawer(targetEl, options);
+      setMounted(true);
+    }
+  }, [isMounted, name])
+
 
   return (
     <>
       <div id={name} className="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-80 dark:bg-gray-800" tabIndex={-1} aria-labelledby="drawer-label">
         <button type="button" data-drawer-hide={name} aria-controls={name} className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
-          <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          <FaTimes size={24} className="text-slate-400 dark:text-slate-50" />
           <span className="sr-only">Close menu</span>
         </button>
         {children}
@@ -27,4 +57,4 @@ const Drawer = ({ name, children, trigger }: DrawerProps) => {
   )
 }
 
-export default Drawer;
+export default CustomDrawer;
