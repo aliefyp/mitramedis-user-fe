9ef8e7b265b1e4ace898ee1ec1b6dd22549f82f8
Alief <aliefyp@gmail.com>
2023-06-17 15:46:58 +0700

    feat: call login api

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,12 @@
 import Input from "components/Input";
 import { useForm } from "react-hook-form";
+// import crypto from "crypto-js";
 // import { useNavigate } from "react-router-dom";
 
 type FormValues = {
   email: string;
   password: string;
-  remember: boolean;
+  // remember: boolean;
 }
 
 const Login = () => {
@@ -13,15 +14,34 @@ const Login = () => {
   // const navigate = useNavigate();
 
   const onSubmit = (val: FormValues) => {
-    console.log(val)
+    // const body = JSON.stringify(val);
+    // const key = process.env.REACT_APP_SIGN_KEY || '';
+    
+    // const encoded = crypto.HmacSHA1(body, key).toString(crypto.enc.Hex);
+
+    // const urlParam = new URLSearchParams(val).toString();
+
+    // console.log(urlParam)
+    
+    fetch(`${process.env.REACT_APP_API_ENDPOINT}/v1/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams(val),
+    }).then(res => res.json())
+      .then(res => console.log(res))
+      .catch(err => {
+        console.log('error', err)
+      });
   }
 
   return (
     <section className="bg-slate-200 dark:bg-slate-800 p-0 sm:p-16 h-screen">
-      <div className="flex bg-white p-2 flex-col sm:flex-row rounded-none sm:rounded-3xl h-auto sm:h-full shadow dark:bg-teal-800">
+      <div className="flex max-w-screen-lg mx-auto bg-white p-2 flex-col sm:flex-row rounded-none sm:rounded-3xl h-auto sm:h-full shadow dark:bg-teal-800">
 
         {/* left section */}
-        <div className="w-full sm:w-1/2 p-4 space-y-4 sm:p-8 bg-gradient-to-r from-brand3 to-brand1 rounded-3xl flex flex-col">
+        <div className="w-full sm:w-1/2 p-4 space-y-4 sm:p-8 bg-gradient-to-b from-brand3 to-brand1 rounded-3xl flex flex-col">
           <div className="flex items-center mb-6 text-2xl font-semibold text-slate-50 dark:text-slate-800">
             <img className="w-8 h-8 mr-2" src="http://placehold.it/32x32" alt="logo" />
             Mitramedis
@@ -30,7 +50,7 @@ const Login = () => {
             <img className="m-0 mx-auto" src="/ilus_login.svg" alt="Login" />
           </div>
           <div className="space-y-4">
-            <h1 className="text-4xl sm:text-5xl font-bold leading tracking-tight text-slate-50 mb-0 sm:mb-12">
+            <h1 className="text-4xl sm:text-5xl font-bold leading tracking-tight text-slate-50 mb-4 sm:mb-12">
               Partner sehat bersama
             </h1>
           </div>
--- src/pages/LupaPassword.tsx
+++ src/pages/LupaPassword.tsx
@@ -3,11 +3,9 @@ import { useForm } from "react-hook-form";
 
 type FormValues = {
   email: string;
-  password: string;
-  remember: boolean;
 }
 
-const Login = () => {
+const LupaPassword = () => {
   const { register, handleSubmit } = useForm<FormValues>()
   // const navigate = useNavigate();
 
@@ -54,35 +52,8 @@ const Login = () => {
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
               </div>
-              <div>
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="••••••••"
-                  {...register('password')}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                />
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      {...register('remember')}
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" 
-                    />
-                  </div>
-                  <div className="ml-3 text-sm">
-                    <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Ingat saya</label>
-                  </div>
-                </div>
-                <a href="#login" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Lupa password?</a>
-              </div>
               <button className="w-full text-slate-50 font-extrabold bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                Login
+                Kirim
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Belum punya akun? <a href="#login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Daftar</a>
@@ -96,4 +67,4 @@ const Login = () => {
   )
 }
 
-export default Login;
+export default LupaPassword;
